0dc19183ffc30f5ff8b6fcdf6743e8aa36b27b88
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-04-21 10:11:12 +0300

    part 8

--- angular-fullstack-project/client/src/app/shared/classes/material.service.ts
+++ angular-fullstack-project/client/src/app/shared/classes/material.service.ts
@@ -2,12 +2,17 @@ import {ElementRef} from "@angular/core";
 
 declare let M: {
   FloatingActionButton: any;
-  toast: (arg0: { html: string }) => void }
+  toast: (arg0: { html: string }) => void
+  updateTextFields(): void;
+}
 export class MaterialService{
   static  toast(message: string){
     M.toast({html: message})
   }
   static  initializeFloatingButton(ref: ElementRef){
     M.FloatingActionButton.init(ref.nativeElement)
+  }
+  static updateTextInputs(){
+    M.updateTextFields()
   }
 }
--- angular-fullstack-project/client/src/app/shared/interfaces.ts
+++ angular-fullstack-project/client/src/app/shared/interfaces.ts
@@ -2,6 +2,9 @@ export interface  User {
   email:string
   password:string
 }
+export interface  Message {
+  message: string
+}
 export interface Category{
   name: string
   imageSrc?: string
